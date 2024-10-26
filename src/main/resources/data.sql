0d588307d6b3970bf800f21c373897271c7d8a9a
Aragajorge <3glasser@gmail.com>
2024-10-25 21:30:00 -0300

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (id, description, done, target_date, username)
+values (10001, 'Learn Python3', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date, username)
+values (10002, 'Learn PHP', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date, username)
+values (10003, 'Learn Java', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date, username)
+values (10004, 'Learn AWS', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date, username)
+values (10005, 'Learn Linux', false, CURRENT_DATE(), 'in28minutes');
